1a4f681fb2388f7756ff03762ad99ce40fe4a15f
TommyZZ8888 <1950442495@qq.com>
2023-11-21 16:10:33 +0800

    logTest

---
+++ demo-exception-handle/src/test/java/com/www/exception/handle/DemoExceptionHandleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.www.exception.handle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoExceptionHandleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
