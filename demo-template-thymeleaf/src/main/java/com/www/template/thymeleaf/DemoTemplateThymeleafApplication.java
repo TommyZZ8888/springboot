26af5aaa415836dbac0c43bc5b2c43ff54520232
TommyZZ8888 <1950442495@qq.com>
2023-11-24 18:01:12 +0800

    jdk 17 sb3

---
+++ demo-template-thymeleaf/src/main/java/com/www/template/thymeleaf/DemoTemplateThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.www.template.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTemplateThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTemplateThymeleafApplication.class, args);
+    }
+
+}
---
+++ demo-template-thymeleaf/src/test/java/com/www/template/thymeleaf/DemoTemplateThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.www.template.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTemplateThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
