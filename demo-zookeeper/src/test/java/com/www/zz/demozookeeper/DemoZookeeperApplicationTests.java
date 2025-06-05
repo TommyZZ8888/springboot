a1afbc7aae8c1b1cff1908c005203bd950433973
卫刚 张 <1950442495@qq.com>
2025-06-05 18:50:04 +0800

    demo-zookeeper

---
+++ demo-zookeeper/src/test/java/com/www/zz/demozookeeper/DemoZookeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.www.zz.demozookeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoZookeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
